6c87c34a418bc329a5d8e1a982682c44f1beea44
WEIFENG-NUSCEG <weifeng5655@hotmail.com>
2019-08-28 22:52:53 +0800

    Update Duke.java
    week2 level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,20 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println("Hello! I'm Duke\n What can I do for you?");
+
+        Scanner ss = new Scanner(System.in);
+
+        String input;
+        while(!(input = ss.nextLine()).equals("bye"))
+        {
+            System.out.println(input);
+        }
+
+        System.out.println("Bye. Hope to see you again soon!");
+
+
+
     }
 }
